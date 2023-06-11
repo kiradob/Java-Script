c804f1e7eab765b650fd410af449a31587fc0261
kira.dob <arik.dobrynina@yandex.ru>
2023-06-11 12:43:48 +0700

    added java script semenar 1 task3

--- java_sem1_task3.java
+++ java_sem1_task3.java
@@ -11,27 +11,33 @@ public class java_sem1_task3 {
         String CommonPrefix=findCommonPrefix(array);
         System.out.println(CommonPrefix);
     }
-
+// Поиск самой короткой строки
     static String findCommonPrefix(String[]array){
-        String first=array[0];
+// Берем первую строку за эталонную
+        String shortString=array[0];
+// Длины всех остальных строк мя сравниваем с длиной первой lenght
         for(int i=1;i<array.length; i++){
-            if (first.length()>array[i].length()){
-                first=array[i];
+            if (array[i].length()<shortString.length()){
+                shortString=array[i];
+// По итогу мы имеем самую короткую строку из всех
             }
         }
         String result="";
-        first: for (int index=0; index<first.length();index++){
-            char firstIndexChar=first.charAt(index);
+// Мы бежим по всем индексам от 0 до длины самой короткой строки lenght
+// сравниваем конкретные символы через индексы с такими же индексами в других строках
+        first: for (int index=0; index<shortString.length();index++){
+            char firstIndexChar=shortString.charAt(index);
             for (int i=1;i<array.length; i++){
 
 
                 char secondStringChar=array[i].charAt(index);
-
+// Если в каких-то строках например 10-й индекс не совпал, значит это уже не общий префикс, поэтому мы останавливаем все и выходим
                 if (firstIndexChar!=secondStringChar){
                     break first;
                 }
             }
-            result+=first.charAt(index);
+// Если какой-то индекс идентичен во всех строках, то мы его записываем в наш результат
+            result+=shortString.charAt(index);
         }
         return result;
     }
